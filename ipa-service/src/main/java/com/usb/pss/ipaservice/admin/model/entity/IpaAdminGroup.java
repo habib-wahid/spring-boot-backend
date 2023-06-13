5053c656576ab3b140f15a4309004499afce6360
Gias Uddin <mdgiashu069@gmail.com>
2023-06-13 15:15:21 +0600

    Added many to many relationship in group and role

--- ipa-service/src/main/java/com/usb/pss/ipaservice/admin/model/entity/IpaAdminGroup.java
+++ ipa-service/src/main/java/com/usb/pss/ipaservice/admin/model/entity/IpaAdminGroup.java
@@ -1,9 +1,13 @@
 package com.usb.pss.ipaservice.admin.model.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -11,6 +15,9 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "ipa_admin_group")
@@ -31,5 +38,13 @@ public class IpaAdminGroup {
     private Long id;
 
     private String name;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "ipa_admin_group_role",
+            joinColumns = @JoinColumn(name = "group_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    Set<IpaAdminRole> assignedRoles = new HashSet<>();
+
     private boolean active = true;
 }
