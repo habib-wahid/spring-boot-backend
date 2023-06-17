3215673fe0a15787eb417fbda0eff99f33737191
Giash Uddin <mdgiashu069@gmail.com>
2023-06-17 10:43:24 +0600

    Rollback IpaServiceApplicationTests

--- ipa-service/src/test/java/com/usb/pss/ipaservice/IpaServiceApplicationTestsName.java
+++ ipa-service/src/test/java/com/usb/pss/ipaservice/IpaServiceApplicationTestsName.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class IpaServiceApplicationTestsName {
+class IpaServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
