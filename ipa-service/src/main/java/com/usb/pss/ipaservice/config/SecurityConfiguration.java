b11a9af0351e7a51f5a71be7225342068135c232
Shourov Foisal <shourovfoisal@gmail.com>
2023-06-19 15:24:47 +0600

    Updated cors

--- ipa-service/src/main/java/com/usb/pss/ipaservice/config/SecurityConfiguration.java
+++ ipa-service/src/main/java/com/usb/pss/ipaservice/config/SecurityConfiguration.java
@@ -14,7 +14,6 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
 import java.util.Collections;
 
 import static org.springframework.http.HttpMethod.POST;
@@ -66,8 +65,8 @@ public class SecurityConfiguration {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedOrigins(Collections.singletonList("*"));
+        configuration.setAllowedMethods(Collections.singletonList("*"));
         configuration.setAllowedHeaders(Collections.singletonList("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
