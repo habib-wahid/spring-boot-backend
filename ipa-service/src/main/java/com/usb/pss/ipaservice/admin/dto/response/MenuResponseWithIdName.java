ce11b158df0f8a3fb0f2acc0d2017896b514aed1
Shazzad Hossain <shazzad.khan@technonext.com>
2023-06-25 15:23:35 +0600

    added all-menu-with-id-name api endpoint

---
+++ ipa-service/src/main/java/com/usb/pss/ipaservice/admin/dto/response/MenuResponseWithIdName.java
@@ -0,0 +1,15 @@
+package com.usb.pss.ipaservice.admin.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MenuResponseWithIdName {
+    private Long id;
+    private String name;
+}
