93c33e3f35b12790821a7b63a2d82d29a0bee3ae
Shazzad Hossain <shazzad.khan@technonext.com>
2023-07-06 13:28:46 +0600

    removed prepareEntity method and create getPasswordPolicyById method

--- ipa-service/src/main/java/com/usb/pss/ipaservice/admin/service/impl/PasswordPolicyServiceImpl.java
+++ ipa-service/src/main/java/com/usb/pss/ipaservice/admin/service/impl/PasswordPolicyServiceImpl.java
@@ -1,10 +1,12 @@
 package com.usb.pss.ipaservice.admin.service.impl;
 
+import static com.usb.pss.ipaservice.common.ExceptionConstant.PASSWORD_POLICY_NOT_FOUND;
+
 import com.usb.pss.ipaservice.admin.dto.request.PasswordPolicyRequest;
 import com.usb.pss.ipaservice.admin.model.entity.PasswordPolicy;
 import com.usb.pss.ipaservice.admin.repository.PasswordPolicyRepository;
 import com.usb.pss.ipaservice.admin.service.iservice.PasswordPolicyService;
-import java.util.List;
+import com.usb.pss.ipaservice.exception.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,30 +18,20 @@ public class PasswordPolicyServiceImpl implements PasswordPolicyService {
 
     @Override
     public void updatePassPolicy(PasswordPolicyRequest request) {
-        PasswordPolicy passwordPolicy;
-        List<PasswordPolicy> passwordPolicies = passwordPolicyRepository.findAll();
+        PasswordPolicy passwordPolicy = getPasswordPolicyById(1L);
 
-        if (!passwordPolicies.isEmpty()) {
-            passwordPolicy = passwordPolicies.get(0);
-            prepareEntity(passwordPolicy, request);
-        } else {
-            passwordPolicy = PasswordPolicy.builder()
-                .passwordLength(request.passwordLength())
-                .containsUppercase(request.containsUppercase())
-                .containsLowercase(request.containsLowercase())
-                .containsDigit(request.containsDigit())
-                .containsSpecialCharacters(request.containsSpecialCharacters())
-                .build();
-        }
-
-        passwordPolicyRepository.save(passwordPolicy);
-    }
-
-    private void prepareEntity(PasswordPolicy passwordPolicy, PasswordPolicyRequest request) {
         passwordPolicy.setPasswordLength(request.passwordLength());
         passwordPolicy.setContainsUppercase(request.containsUppercase());
         passwordPolicy.setContainsLowercase(request.containsLowercase());
         passwordPolicy.setContainsDigit(request.containsDigit());
         passwordPolicy.setContainsSpecialCharacters(request.containsSpecialCharacters());
+
+        passwordPolicyRepository.save(passwordPolicy);
+    }
+
+    @Override
+    public PasswordPolicy getPasswordPolicyById(Long id) {
+        return passwordPolicyRepository.findById(id)
+            .orElseThrow(() -> new ResourceNotFoundException(PASSWORD_POLICY_NOT_FOUND));
     }
 }
