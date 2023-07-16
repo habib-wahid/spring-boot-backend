333a48fc2d32f930f42da906f24bf9eb35b3bba0
Shazzad Hossain <shazzad.khan@technonext.com>
2023-07-16 17:47:30 +0600

    replaced PutMapping with PatchMapping

--- ipa-service/src/main/java/com/usb/pss/ipaservice/admin/controller/UserController.java
+++ ipa-service/src/main/java/com/usb/pss/ipaservice/admin/controller/UserController.java
@@ -13,6 +13,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -72,7 +73,7 @@ public class UserController {
         userService.updateUserRole(userRoleActionRequest);
     }
 
-    @PutMapping("/updateUserStatus")
+    @PatchMapping("/updateUserStatus")
     @Operation(summary = "Update user activation status")
     public void updateUserStatus(@Validated @RequestBody UserStatusRequest userStatusRequest) {
         userService.updateUserStatusInfo(userStatusRequest);
