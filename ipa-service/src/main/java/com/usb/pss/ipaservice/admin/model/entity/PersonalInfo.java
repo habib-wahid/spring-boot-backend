8b904cf881b811de37b737e91b89d1852315e53b
Arka Bhuiyan <arka.bhuiyan@technonext.com>
2023-07-20 10:09:10 +0600

    [PSS-115] fixed the relationship mapping between currency and personal info.

--- ipa-service/src/main/java/com/usb/pss/ipaservice/admin/model/entity/PersonalInfo.java
+++ ipa-service/src/main/java/com/usb/pss/ipaservice/admin/model/entity/PersonalInfo.java
@@ -3,6 +3,9 @@ package com.usb.pss.ipaservice.admin.model.entity;
 import com.usb.pss.ipaservice.common.model.BaseAuditorEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -34,5 +37,10 @@ public class PersonalInfo extends BaseAuditorEntity {
     private String pointOfSales;
     private String accessLevel;
     private String airport;
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+        name = "adm_personal_info_currency_list",
+        joinColumns = @JoinColumn(name = "personal_info_id"),
+        inverseJoinColumns = @JoinColumn(name = "currency_id"))
     private List<Currency> allowedCurrencies;
 }
