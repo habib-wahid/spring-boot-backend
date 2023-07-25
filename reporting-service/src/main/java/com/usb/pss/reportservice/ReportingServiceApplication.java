4c824a2a554e858866c0a986068a9b34e39af5af
Arka Bhuiyan <arka.bhuiyan@technonext.com>
2023-07-25 11:21:27 +0600

    [PSS-115] fixed the formatting of other application main files.

--- reporting-service/src/main/java/com/usb/pss/reportservice/ReportingServiceApplication.java
+++ reporting-service/src/main/java/com/usb/pss/reportservice/ReportingServiceApplication.java
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ReportingServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ReportingServiceApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(ReportingServiceApplication.class, args);
 
-		System.out.println("Hello from Reporting Service");
-	}
+        System.out.println("Hello from Reporting Service");
+    }
 
 }
