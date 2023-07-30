9bb32bde79a5c3350e1928d7ca50a9c003bd9b1c
Arka Bhuiyan <arka.bhuiyan@technonext.com>
2023-07-30 17:58:14 +0600

    [PSS-293] set the size of user_code to be of 6 digits.

--- ipa-service/src/main/resources/db/changelog/SQL/293_profile_information_initialize_data.sql
+++ ipa-service/src/main/resources/db/changelog/SQL/293_profile_information_initialize_data.sql
@@ -4,7 +4,7 @@ ALTER TABLE adm_user ADD COLUMN
     if not exists is2fa_enabled BOOLEAN;
 
 ALTER TABLE adm_user ADD COLUMN
-    IF NOT EXISTS user_code VARCHAR(255);
+    IF NOT EXISTS user_code VARCHAR(6);
 
 update adm_user set is2fa_enabled = false
 where id = 1;
