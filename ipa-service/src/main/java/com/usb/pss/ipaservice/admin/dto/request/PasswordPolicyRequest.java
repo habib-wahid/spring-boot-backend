1a6e130b97f0932ecb1f513c29d80a32227e59b3
Shazzad Hossain <shazzad.khan@technonext.com>
2023-08-01 12:17:00 +0600

    added Min constraint in password policy request

--- ipa-service/src/main/java/com/usb/pss/ipaservice/admin/dto/request/PasswordPolicyRequest.java
+++ ipa-service/src/main/java/com/usb/pss/ipaservice/admin/dto/request/PasswordPolicyRequest.java
@@ -1,9 +1,11 @@
 package com.usb.pss.ipaservice.admin.dto.request;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
 public record PasswordPolicyRequest(
     @NotNull
+    @Min(value = 4, message = "Password length must not be less than 4")
     Integer passwordLength,
     @NotNull
     Boolean containsUppercase,
